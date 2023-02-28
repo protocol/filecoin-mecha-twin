8b9012c290b440ceb66532542c1300ed53698f27
Maria Silva <misilva73@gmail.com>
2023-02-28 11:11:55 +0100

    Update innit version

--- mechafil/__init__.py
+++ mechafil/__init__.py
@@ -1,6 +1,6 @@
 """MechaFIL: Mechanistic model for the Filecoin Economy"""
 
-__version__ = "1.2"
+__version__ = "1.7"
 __author__ = "Maria Silva <misilva73@gmail.com>, Tom Mellan <t.mellan@imperial.ac.uk>, Kiran Karra <kiran.karra@gmail.com>"
 __all__ = []
 
